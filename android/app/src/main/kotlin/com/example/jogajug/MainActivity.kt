c087e32d3d498c5113273e9321a6febe07ddc088
gblbd <gbl.devs@gmail.com>
2023-08-29 18:53:36 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/jogajug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jogajug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Pages/Friends.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Friends extends StatefulWidget {
+  const Friends({super.key});
+
+  @override
+  State<Friends> createState() => _FriendsState();
+}
+
+class _FriendsState extends State<Friends> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Friends'
+        ),
+      ),
+    );;
+  }
+}
